42e71db4094cb466544676f33d17044b6920c93b
UmidjonDeveloper <umidzhonfullstackdev@gmail.com>
2022-03-30 07:28:31 +0600

    Happy App Web Site

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const nav = document.querySelector(".navbar__main");
 const navOpenBtn = document.querySelector(".navbar--open--icon");
 const navCloseBtn = document.querySelector(".navbar--close--icon");
+const navLinks = document.querySelectorAll(".nav__link")
 
 navOpenBtn.addEventListener('click', () => {
     nav.classList.add("open");
@@ -10,3 +11,8 @@ navOpenBtn.addEventListener('click', () => {
 navCloseBtn.addEventListener('click', () => {
     nav.classList.remove("open");
 });
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.remove("open");
+    });
+})
